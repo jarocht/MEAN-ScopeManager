ed895b5ad32f48be6136a3e22790b9ba9b520bb1
Tim Jaroch <tim.jaroch@inin.com>
2015-04-29 17:55:54 -0400

    Fixed defect in hours forecasting logic. Added service install and uninstall functionality via node-windows.

---
+++ service-installer.js
@@ -0,0 +1,18 @@
+﻿var Service = require('node-windows').Service;
+
+var svc = new Service({
+    name: 'PreSales ScopeManager',
+    description: 'The PreSales ScopeManager WebService.',
+    script: require('path').join(__dirname, 'server.js')
+});
+
+svc.on('install', function () {
+    svc.start();
+});
+
+svc.on('uninstall', function () {
+    console.log('Uninstall complete.');
+    console.log('The service exists: ', svc.exists);
+});
+
+svc.install();
---
+++ service-uninstaller.js
@@ -0,0 +1,14 @@
+﻿var Service = require('node-windows').Service;
+
+var svc = new Service({
+    name: 'PreSales ScopeManager',
+    description: 'The PreSales ScopeManager WebService.',
+    script: require('path').join(__dirname, 'server.js')
+});
+
+svc.on('uninstall', function () {
+    console.log('Uninstall complete.');
+    console.log('The service exists: ', svc.exists);
+});
+
+svc.uninstall();
